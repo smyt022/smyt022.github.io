0b6963d7521eb5fccd12e7eb34f2a1adcf385526
smyt022 <tocatsa@gmail.com>
2022-03-27 18:04:58 -0400

    adding things to the canvas

--- pong.js
+++ pong.js
@@ -2,26 +2,51 @@
 const canvas = document.getElementById("pong");
 const context = canvas.getContext("2d"); 
 
-//draw a rectangle
-context.fillStyle = "black";
-context.fillRect(100, 200,50,75);
-
-//draw a circle
-context.fillStyle = "red";
-context.beginPath();
-context.arc(300,350,100,0, Math.PI*2, false);
-
-context.closePath();
-context.fill();
+//canvas background color
+drawRect(0, 0, 600, 400, "black");
 
 
-//rectangle draw function
+//player 1 paddle
+const playerOne = {
+    x: 0,
+    y: canvas.height/2 -50,
+    width:10,
+    height: 100,
+    color: "WHITE",
+    score: 0
+}
+
+drawRect(playerOne.x, playerOne.y, playerOne.width, playerOne.height, playerOne.color);
+
+//player 2 paddle
+const playerTwo = {
+    x: canvas.width-1,
+    y: canvas.height/2 -50,
+    width: 10,
+    height: 100,
+    color: "WHITE",
+    score: 0
+}
+drawRect(playerTwo.x, playerTwo.y, playerTwo.width, playerTwo.height, playerTwo.color);
+
+//net in the middle
+const  net = {
+    x: canvas.width/2 -2,
+    y: 0,
+    width:2,
+    height: 10,
+    color: "WHITE",
+}
+drawNet();
+
+
+//rectangle draw FUNCTION
 function drawRect(x,y,w,h, color){
     context.fillStyle = color;
     context.fillRect(x,y,w,h);
 }
 
-//circle draw function
+//circle draw FUNCTION
 function drawCircle(x,y,r, color){
     ctx.fillStyle = color;
     ctx.beginPath();
@@ -30,9 +55,18 @@ function drawCircle(x,y,r, color){
     ctx.fill();
 }
 
-//draw text function
+//draw text FUNCTION
 function drawText(text, x,y, color){
     ctx.fillStyle = color;
     ctx.font = "75px fantasy";
     ctx.fillText(text, x, y);
+}
+
+//draw net FUNCTION
+function drawNet(){ //all the little rectangles
+    for(let i=0; i<= canvas.height; i+=15){
+        //as the i increases, you go down the canvas
+        drawRect(net.x,net.y + i, net.width, net.height, net.color);
+
+    }
 }
